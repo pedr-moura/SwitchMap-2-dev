a4920a9c450de601f77ed64c87f74c2da197b03b
Pedro Moura <105287149+pedr-moura@users.noreply.github.com>
2025-03-24 11:54:47 -0300

    Update init-server.bat

--- init-server.bat
+++ init-server.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /k "cd /d A:\SwitchMap 1.0\backend\websocket && py websc.py"
-start cmd /k "cd /d C:\ENTUITY && node scraper.js"
-start cmd /k "cd /d D:\CACHEMAPA\cacheMapa && node cachemap.js"
+start cmd /k "cd /d LOCAL && py websc.py"
+start cmd /k "cd /d LOCAL && node scraper.js"
+start cmd /k "cd /d LOCAL && node cachemap.js"
 exit
