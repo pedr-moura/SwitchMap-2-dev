142faa0426d2dc722950b2d24e7acb95d216c2df
Pedro Moura <105287149+pedr-moura@users.noreply.github.com>
2025-04-15 14:31:54 -0300

    Update scriptDados.js

--- frontend/source/script/scriptDados.js
+++ frontend/source/script/scriptDados.js
@@ -704,7 +704,7 @@ function atualizarMarcadores(hosts) {
     justify-content: center;">
                         <b class="nomedosw" style="color: ${ponto.ativo}; font-size: 14px;">${ponto.nome}</b>
                         <br>
-                        <span class="latitude" style="text-align: center; width: 100%; opacity: 0.7; font-size: 12px;">${ponto.local}</span>
+                        <span class="latitude" style="text-align: center; width: 100%; opacity: 0.7; font-size: 12px;">${ponto.ip}</span>
                         <span style="color: #6c757d; font-style: italic; font-size: 12px; width: 100%;
     text-align: center;">"${ponto.observacao}"</span>
                         ${info}
